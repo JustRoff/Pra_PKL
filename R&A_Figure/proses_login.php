8f5631b4164d1ade8ac00e8ee92309a4507b1658
JustRoff <rofidwis123@gmail.com>
2025-03-18 10:36:00 +0700

    Add files via upload

---
+++ R&A_Figure/proses_login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql_admin = "Select * from admin where username='$username' and password = md5('$password') ";
+$query_admin = mysqli_query($koneksi,$sql_admin);
+
+$sql_users = "Select * from users where username='$username' and password = md5('$password') ";
+$query_users = mysqli_query($koneksi,$sql_users);
+
+if (mysqli_num_rows($query_admin)) {
+    $admin = mysqli_fetch_assoc($query_admin);
+    $_SESSION['id_admin'] = $admin['id_admin'];
+    header("location:index.php?Login=S");
+    exit;
+} elseif (mysqli_num_rows($query_users)) {
+    $_SESSION['users'] = $username;
+    header("location:index.php?Login=S");
+    exit;
+} else {
+    header("Location:login.php?Login=G");
+    exit;
+}
+?>
