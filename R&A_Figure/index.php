b44fd7aab6c7c6814c91b8e22093a5a038f7d238
JustRoff <rofidwis123@gmail.com>
2025-03-25 22:48:52 +0700

    Add files via upload

---
+++ R&A_Figure/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="dashboard.php">P</a><br>
+
+    <a href="logout.php" onclick="return confirm('Apakah admin <?= $_SESSION['admin'] ?> ingin logout?')">
+    <button>Logout</button>
+</a><br>
+
+<a href="produk.php">produk</a>
+
+</body>
+</html>
